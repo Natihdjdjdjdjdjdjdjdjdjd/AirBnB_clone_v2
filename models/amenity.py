cd46ac32b5d55d32be1c8fac5588558ba417fe97
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-02-17 20:47:37 -0800

    models

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,15 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
+amenity.py
+#!/usr/bin/python3
+"""let amen our function of python"""
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+    """class for Amenity
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
